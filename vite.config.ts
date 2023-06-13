65fe00fb80edafe73895c081b6a3781dbedc0722
butterflydreams <inbutterflydream@gmail.com>
2023-06-13 17:20:50 +0800

    add: config route

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@configs": fileURLToPath(new URL("./src/configs", import.meta.url))
     }
   }
 });
