81bb073db2e8cdacef32a3154728e022196c6695
butterflydreams <inbutterflydream@gmail.com>
2023-06-13 19:47:16 +0800

    add: font default style

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,37 @@
 <style scoped>
+h1 {
+  font-size: 32px;
+  font-weight: 900;
+}
+h2 {
+  font-size: 24px;
+  font-weight: 800;
+}
+h3 {
+  font-size: 18.72px;
+  font-weight: 700;
+}
+h4 {
+  font-size: 16px;
+  font-weight: 600;
+}
+h5 {
+  font-size: 13.28px;
+  font-weight: 500;
+}
+h6 {
+  font-size: 12px;
+  font-weight: 400;
+}
 </style>
 
 <template>
-  <h1>Hello, guys! this is a tools page!</h1>
+  <h1>h1: Hello, guys! this is a tools page!</h1>
+  <h2>h2: This is h2 data.</h2>
+  <h3>h3: This is h3 data.</h3>
+  <h4>h4: This is h4 data.</h4>
+  <h5>h5: This is h5 data.</h5>
+  <h6>h6: This is h6 data.</h6>
 </template>
 
 <script setup lang="ts">
