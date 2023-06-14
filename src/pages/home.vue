6b2b8f5e228e6ea48c0bda61bf713df3d19d18fd
butterflydreams <inbutterflydream@gmail.com>
2023-06-14 17:56:12 +0800

    add: less parser

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,4 @@
-<style scoped>
+<style lang="less" scoped>
 h1 {
   font-size: 32px;
   font-weight: 900;
@@ -23,6 +23,25 @@ h6 {
   font-size: 12px;
   font-weight: 400;
 }
+.wrapper {
+  position: relative;
+  margin: 100px auto;
+  width: 100%;
+  .btn-button {
+    display: block;
+    position: relative;
+    margin: 0 auto;
+    width: 150px;
+    height: 80px;
+    background-image: linear-gradient(90deg, #2cd3d7 0%, #2acf6f 100%);
+    line-height: 80px;
+    letter-spacing: 0;
+    font-size: 32px;
+    font-weight: 600;
+    text-align: center;
+    color: #ffffff;
+  }
+}
 </style>
 
 <template>
@@ -32,7 +51,13 @@ h6 {
   <h4>h4: This is h4 data.</h4>
   <h5>h5: This is h5 data.</h5>
   <h6>h6: This is h6 data.</h6>
+  <div class="wrapper">
+    <a class="btn-button" href="javascript:;" @click="OnButtonClick">Click Me!</a>
+  </div>
 </template>
 
 <script setup lang="ts">
+const OnButtonClick = function () {
+  console.log("You have clicked a button!");
+};
 </script>
